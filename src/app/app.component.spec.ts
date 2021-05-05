440ed3d6b4eefe3bbd7e322cf659ed8f04a7bfb8
gurleen7 <gurleen.virdi@qtsolv.com>
2021-05-05 16:17:56 +0530

    added test dependency

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'githubaction'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('githubaction1');
+    expect(app.title).toEqual('githubaction');
   });
 
   it('should render title', () => {
